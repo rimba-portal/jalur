b209e047b5a7d2bc38830f15aef6a82f0c1155fe
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-27 10:12:48 +0800

    Update 2026-07-27 10:12

--- src/MenuServiceProvider.php
+++ src/MenuServiceProvider.php
@@ -6,18 +6,19 @@ namespace Rimba\Menu;
 
 use Rimba\Base\Services\BitesServiceProvider;
 
+
 class MenuServiceProvider extends BitesServiceProvider
 {
-    protected string $configFile = __DIR__.'/../config/bites.php';
+    protected string $configFile = __DIR__ . '/../config/bites.php';
 
     protected function bootPackage(): void
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         //
     }
-
     protected function registerPackage(): void
     {
         //
     }
+
 }
